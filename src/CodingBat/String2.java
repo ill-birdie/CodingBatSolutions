cb68edf60e751d438f37b6cbfba0d64270c4605b
Ava <avahom16@gmail.com>
2026-01-30 08:05:10 -0500

    Better file organization

---
+++ src/CodingBat/String2.java
@@ -0,0 +1,134 @@
+package CodingBat;
+
+public final class String2 {
+    private String2() { }
+
+    public static String doubleChar(String str) {
+        StringBuilder doubledWord = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            doubledWord.append(currChar);
+            doubledWord.append(currChar);
+        }
+        return doubledWord.toString();
+    }
+
+    public static int countCode(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 3; i++) {
+            String currWindow = str.substring(i, i + 4);
+            if (currWindow.matches("co.e")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean bobThere(String str) {
+        for (int i = 0; i < str.length() - 2; i++) {
+            char currChar = str.charAt(i);
+            char nextNextChar = str.charAt(i + 2);
+            if (currChar == nextNextChar) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String repeatFront(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String currWindow = str.substring(0, n - i);
+            result.append(currWindow);
+        }
+        return result.toString();
+    }
+
+    public static String repeatEnd(String str, int n) {
+        String lastChars = str.substring(str.length() - n);
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            result.append(lastChars);
+        }
+        return result.toString();
+    }
+
+    public static boolean prefixAgain(String str, int n) {
+        String prefix = str.substring(0, n);
+        String regex = prefix + ".*" + prefix + ".*";
+        return str.matches(regex);
+    }
+
+    public static boolean sameStarChar(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            boolean firstOrLast = (i == 0 || i == str.length() - 1);
+            char currChar = str.charAt(i);
+            if (currChar == '*' && !firstOrLast) {
+                char prevChar = str.charAt(i - 1);
+                char nextChar = str.charAt(i + 1);
+                if (prevChar != nextChar) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            String currWindow = str.substring(i, i + 2);
+            if (currWindow.equals("hi")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean endOther(String a, String b) {
+        if (a.length() < b.length()) {
+            String temp = a;
+            a = b;
+            b = temp;
+        }
+        int targetStartIndex = a.length() - b.length();
+        return a.substring(targetStartIndex).equalsIgnoreCase(b);
+    }
+
+    public static boolean xyBalance(String str) {
+        boolean hangingX = false;
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            if (currChar == 'x') {
+                hangingX = true;
+            } else if (currChar == 'y') {
+                hangingX = false;
+            }
+        }
+        return !hangingX;
+    }
+
+    public static boolean xyzMiddle(String str) {
+        int xyzStartIndex = str.substring(str.length() / 2).indexOf("xyz");
+        if (xyzStartIndex == -1) {
+            return false;
+        }
+        int difference = str.substring(0, xyzStartIndex).length() - str.substring(xyzStartIndex + 3).length();
+        return -1 <= difference && difference <= 1;
+    }
+
+    public static boolean xyzThere(String str) {
+        return str.matches(".*(?<!\\.)xyz.*");
+    }
+
+    public String oneTwo(String str) {
+        StringBuilder result = new StringBuilder();
+        while (str.length() >= 3) {
+            String currWindow = str.substring(0, 3);
+            String newWord = currWindow.substring(1) + currWindow.charAt(0);
+            result.append(newWord);
+            str = str.substring(3);
+        }
+        return result.toString();
+    }
+}
---
+++ src/CodingBat/String3.java
@@ -0,0 +1,36 @@
+package CodingBat;
+
+public final class String3 {
+    private String3() {
+    }
+
+    public static int countYZ(String str) {
+        int count = 0;
+        int startIdx = 0;
+        while (!str.substring(startIdx, startIdx + 1).matches("[A-Za-z]")) {
+            startIdx++;
+        }
+        str = str.substring(startIdx);
+        String[] words = str.split("[^A-Za-z]+");
+        for (String w : words) {
+            String lastLetter = w.substring(w.length() - 1);
+            if ("YyZz".contains(lastLetter)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String withoutString(String base, String remove) {
+        int idx = 0;
+        while (idx < base.length() - remove.length()) {
+            String currWindow = base.substring(idx, idx + remove.length());
+            if (currWindow.equals(remove)) {
+                base = base.substring(idx) + base.substring(idx + remove.length());
+            } else {
+                idx++;
+            }
+        }
+        return base;
+    }
+}
