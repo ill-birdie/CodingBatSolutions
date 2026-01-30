805fae8132f5a6982fba67465bc3bf0dd82f409d
Ava <avahom16@gmail.com>
2026-01-29 19:32:30 -0500

    Completed String-2 countHi

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -123,5 +123,16 @@ public final class CodingBat {
             }
             return true;
         }
+
+        public static int countHi(String str) {
+            int count = 0;
+            for (int i = 0; i < str.length() - 1; i++) {
+                String currWindow = str.substring(i, i + 2);
+                if (currWindow.equals("hi")) {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
