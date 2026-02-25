7e71effbe5f51a507f61aa4f49aa01cd91632781
Ava <avahom16@gmail.com>
2026-02-25 18:07:04 -0500

    Arrays3 canBalance and maxSpan

--- src/CodingBat/Arrays3.java
+++ src/CodingBat/Arrays3.java
@@ -1,7 +1,41 @@
 package CodingBat;
 
-public class Arrays3 {
+public final class Arrays3 {
     private Arrays3() { }
+
+    public static int maxSpan(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        int result = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int currNum = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == currNum) {
+                    result = Math.max(result, (j + 1) - i);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean canBalance(int[] nums) {
+        for (int split = 0; split < nums.length; split++) {
+            int firstHalfSum = 0;
+            for (int i = 0; i < split; i++) {
+                firstHalfSum += nums[i];
+            }
+            int secondHalfSum = 0;
+            for (int i = split; i < nums.length; i++) {
+                secondHalfSum += nums[i];
+            }
+            if (firstHalfSum == secondHalfSum) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static int countClumps(int[] nums) {
         int count = 0;
