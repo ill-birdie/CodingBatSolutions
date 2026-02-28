abd147f0a9057d245ca431f7b290c75688e884a6
Ava <avahom16@gmail.com>
2026-02-27 19:04:29 -0500

    Array1 front11

--- src/CodingBat/Array1.java
+++ src/CodingBat/Array1.java
@@ -72,4 +72,15 @@ public final class Array1 {
         nums[2] = maxValue;
         return nums;
     }
+
+    public static int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length == 0) {
+            return new int[] {};
+        } else if (b.length == 0) {
+            return new int[] {a[0]};
+        } else if (a.length == 0) {
+            return new int[] {b[0]};
+        }
+        return new int[] {a[0], b[0]};
+    }
 }
