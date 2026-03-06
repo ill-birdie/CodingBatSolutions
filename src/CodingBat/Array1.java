7b3debcb185de2975695fc7826723b72a8251515
Ava <avahom16@gmail.com>
2026-03-06 08:08:40 -0500

    Array1 reorganization, make2, frontPiece, maxTriple, midTHree, swapEnds, plusTwo, makeMiddle

--- src/CodingBat/Array1.java
+++ src/CodingBat/Array1.java
@@ -3,16 +3,33 @@ package CodingBat;
 public final class Array1 {
     private Array1() { }
 
-    public boolean firstLast6(int[] nums) {
+    public static boolean firstLast6(int[] nums) {
         return (nums[0] == 6 || nums[nums.length - 1] == 6);
     }
 
-    public boolean commonEnd(int[] a, int[] b) {
+    public static boolean sameFirstLast(int[] nums) {
+        if (nums.length < 1) return false;
+        return nums[0] == nums[nums.length - 1];
+    }
+
+    public static boolean commonEnd(int[] a, int[] b) {
         return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
+    public static int sum3(int[] nums) {
+        return nums[0] + nums[1] + nums[2];
     }
 
     public static int[] reverse3(int[] nums) {
         return new int[] {nums[2], nums[1], nums[0]};
+    }
+
+    public static int[] maxEnd3(int[] nums) {
+        int maxValue = Math.max(nums[0], nums[2]);
+        nums[0] = maxValue;
+        nums[1] = maxValue;
+        nums[2] = maxValue;
+        return nums;
     }
 
     public static int[] middleWay(int[] a, int[] b) {
@@ -37,9 +54,37 @@ public final class Array1 {
         return new int[] {nums[halfwayHigher - 1], nums[halfwayHigher]};
     }
 
+    public static int[] plusTwo(int[] a, int[] b) {
+        return new int[] {a[0], a[1], b[0], b[1]};
+    }
+
+    public static int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        return nums;
+    }
+
     public static int[] midThree(int[] nums) {
         int middleIdx = nums.length / 2;
         return new int[] {nums[middleIdx - 1], nums[middleIdx], nums[middleIdx + 1]};
+    }
+
+    public static int maxTriple(int[] nums) {
+        return Math.max(
+                nums[nums.length - 1],
+                Math.max(nums[0], nums[nums.length / 2])
+        );
+    }
+
+    public static int[] frontPiece(int[] nums) {
+        if (nums.length >= 2) {
+            return new int[] {nums[0], nums[1]};
+        } else if (nums.length == 1) {
+            return new int[] {nums[0]};
+        } else {
+            return new int[] {};
+        }
     }
 
     public static boolean unlucky1(int[] nums) {
@@ -51,26 +96,21 @@ public final class Array1 {
             return true;
         } else if (nums[0] == 1 && nums[1] == 3) {
             return true;
+        } else if (nums[1] == 1 && nums[2] == 3) {
+            return true;
         } else {
-            return nums[1] == 1 && nums[2] == 3;
+            return false;
         }
     }
 
-    public static boolean sameFirstLast(int[] nums) {
-        if (nums.length < 1) return false;
-        return nums[0] == nums[nums.length - 1];
-    }
-
-    public static int sum3(int[] nums) {
-        return nums[0] + nums[1] + nums[2];
-    }
-
-    public static int[] maxEnd3(int[] nums) {
-        int maxValue = Math.max(nums[0], nums[2]);
-        nums[0] = maxValue;
-        nums[1] = maxValue;
-        nums[2] = maxValue;
-        return nums;
+    public static int[] make2(int[] a, int[] b) {
+        if (a.length >= 2) {
+            return new int[] {a[0], a[1]};
+        } else if (a.length == 1) {
+            return new int[] {a[0], b[0]};
+        } else {
+            return new int[] {b[0], b[1]};
+        }
     }
 
     public static int[] front11(int[] a, int[] b) {
