1281bf2c7ecda2fbcefc33b397625d20150384d9
Ava <avahom16@gmail.com>
2026-03-14 22:59:13 -0400

    Logic2 reformatted; now includes makeBricks, noTeenSum

--- src/CodingBat/Logic2.java
+++ src/CodingBat/Logic2.java
@@ -3,47 +3,46 @@ package CodingBat;
 public final class Logic2 {
     private Logic2() { }
 
+    public static boolean makeBricks(int small, int big, int goal) {
+        return (goal <= small + big*5) && (goal % 5 <= small);
+    }
+
     public static int loneSum(int a, int b, int c) {
-        if (a == b && b == c) {
-            return 0;
-        } else if (a == c) {
-            return b;
-        } else if (b == c) {
-            return a;
-        } else if (a == b) {
-            return c;
-        } else {
-            return a + b + c;
-        }
+        if (a == b && b == c) return 0;
+        if (a == c) return b;
+        if (b == c) return a;
+        if (a == b) return c;
+        return a + b + c;
     }
 
     public static int luckySum(int a, int b, int c) {
-        int sum = 0;
-        int pointer = 0;
-        int[] nums = {a, b, c};
-        while (pointer < nums.length) {
-            int currNum = nums[pointer];
-            if (currNum == 13) {
-                break;
-            }
-            sum += currNum;
-            pointer++;
-        }
-        return sum;
+        if (a == 13) return 0;
+        if (b == 13) return a;
+        if (c == 13) return a + b;
+        return a + b + c;
+    }
+
+    public static int fixTeen(int n) {
+        return ( (13 <= n && n <= 19) && (n != 15 && n != 16) ) ? 0 : n;
+    }
+
+    public static int noTeenSum(int a, int b, int c) {
+        return fixTeen(a) + fixTeen(b) + fixTeen(c);
     }
 
     public static int round10(int num) {
-        double tenthOfNum = (double) num / 10;
-        return (int) Math.round(tenthOfNum) * 10;
+        return (int) Math.round((double) num / 10) * 10;
     }
 
     public static int roundSum(int a, int b, int c) {
-        int[] nums = {a, b, c};
-        int sum = 0;
-        for (int n : nums) {
-            sum += round10(n);
-        }
-        return sum;
+        return round10(a) + round10(b) + round10(c);
+    }
+
+    public static boolean closeFar(int a, int b, int c) {
+        // If b/c is close, return whether c/b is far
+        if (Math.abs(b - a) <= 1) return (Math.abs(c - a) >= 2 && Math.abs(c - b) >= 2);
+        if (Math.abs(c - a) <= 1) return (Math.abs(b - a) >= 2 && Math.abs(b - c) >= 2);
+        return false;
     }
 
     public static int[] bubbleSort(int[] nums) {
